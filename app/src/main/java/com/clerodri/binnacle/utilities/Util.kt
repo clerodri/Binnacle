ecee94b273a2d606f997d9daa265ec6ecad730cf
Ronaldo Rodriguez <rrodriguez@kvz.cl>
2025-01-31 08:38:58 -0500

    home screen design 80%

--- app/src/main/java/com/clerodri/binnacle/utilities/Util.kt
+++ app/src/main/java/com/clerodri/binnacle/utilities/Util.kt
@@ -1,11 +1,15 @@
 package com.clerodri.binnacle.utilities
 
+import android.location.Location
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -33,8 +37,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -45,6 +51,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.clerodri.binnacle.auth.presentation.admin.AdminViewModel
 import com.clerodri.binnacle.ui.theme.BackGroundAppColor
+import com.clerodri.binnacle.ui.theme.Primary
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun IdentifierField(identifier:String, identifierError:String?, onIdentifierChange: (String) -> Unit){
@@ -289,4 +302,96 @@ fun SettingIcon(modifier: Modifier, adminViewModel: AdminViewModel) {
     }
 
 
+}
+
+
+/*
+    //                Spacer(modifier = Modifier.height(16.dp))
+    // GoogleMapComponent()
+//                    Text(text = "Ruta LATITUDE ${viewModel.currentLocation?.latitude}")
+//                    Text(text = "Ruta LONGITUDE ${viewModel.currentLocation?.longitude}")
+//                    viewModel.currentLocation?.let { GoogleMapComponent(location = it) }
+
+
+//            LazyColumn(
+//                modifier = Modifier.fillMaxSize(),
+//                horizontalAlignment = Alignment.CenterHorizontally,
+//                contentPadding = PaddingValues(top = contentPadding.calculateTopPadding()),
+//                verticalArrangement = Arrangement.spacedBy(16.dp),
+//            ) {
+//                item {
+//                    HomeTimerComponent(modifier = Modifier.fillMaxWidth())
+//                    HomeDividerTextComponent()
+//                }
+//                items(10){
+//                    Box(
+//                        modifier = Modifier
+//                            .padding(horizontal = 16.dp)
+//                            .heightIn(100.dp)
+//                            .fillMaxWidth()
+//                            .clip(RoundedCornerShape(20.dp))
+//                            .background(Primary)
+//                    )
+//
+//                    {
+//                        Spacer(modifier = Modifier.height(16.dp))
+//                    }
+//                }
+//
+//            }
+*/
+
+
+
+@Composable
+fun RouteItemComponent(modifier: Modifier = Modifier, value: String) {
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 50.dp)
+            .heightIn(100.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(20.dp))
+            .background(Primary)
+    )
+
+    {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center)
+        ) {
+            Text(
+                text = value,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterHorizontally),
+                fontSize = 26.sp,
+                fontStyle = FontStyle.Italic,
+                color = Color.LightGray,
+                fontFamily = FontFamily.Monospace
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+
+    }
+
+}
+
+
+@Composable
+fun GoogleMapComponent(modifier: Modifier = Modifier, location: Location) {
+    val singapore = LatLng(location.latitude, location.longitude)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 16f)
+    }
+    GoogleMap(
+        modifier = modifier,
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = MarkerState(position = singapore),
+            title = "Laguna dorada",
+            snippet = "Marker in Laguna dorada"
+        )
+    }
 }
